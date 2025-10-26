57a9954e3a95b93aec88805ef0e98c61ac282098
Evany Fernanda <147280313+evanyfe@users.noreply.github.com>
2025-10-26 20:24:18 -0300

    ajuste da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  (import.meta as any)?.env?.VITE_API_BASE_URL || 'https://back-projeto-full.vercel.app/';
+  (import.meta as any)?.env?.VITE_API_BASE_URL || 'https://back-projeto-full.vercel.app';
 
 export const ENDPOINTS = {
   products: '/products',
